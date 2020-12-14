3d0c62b7eecb1334a7e6fe6c95cdeea6869678c4
Isaac Adetona <adetonaisaac12345@gmail.com>
2020-12-14 10:18:35 -0800

    navbar project complete

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
+
 const Review = () => {
 	const [index, setIndex] = useState(0);
 	const {name, job, image, text} = people[index]
